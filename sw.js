7b7fa278bcf41bb21305241ff268b001457037fa
sporttactic <80244896+sporttactic@users.noreply.github.com>
2026-08-13 14:27:05 +0200

    dd

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
 
    BUMP `VERSION` and the matching `?v=` entries in SHELL whenever an asset changes.
 */
-const VERSION = 'v162';
+const VERSION = 'v163';
 const CACHE = 'sporttactic-' + VERSION;
 
 const SHELL = [
